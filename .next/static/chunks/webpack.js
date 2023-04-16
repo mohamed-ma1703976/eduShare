97b3eba52b8fae0c7372380855a99040a9e5e10c
Mohamed Abdelmoety <88920277+mohamed-ma1703976@users.noreply.github.com>
2023-04-16 11:51:25 +0300

    Update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d902c1440c06ea6a"; }
+/******/ 		__webpack_require__.h = function() { return "f450bf9c77899f28"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- component/UpdateForm.js
+++ component/UpdateForm.js
@@ -6,166 +6,161 @@ import {
     RadioGroup,
     FormControlLabel,
     FormControl,
-    FormLabel
-
-} from '@mui/material';
-import { color } from '@mui/system';
-
-import LibraryAddCheckIcon from '@mui/icons-material/LibraryAddCheck';
-
-import React, { useEffect, useState } from 'react'
-import Router, { useRouter } from 'next/router';
-
-const UpdateForm = ({ ids, titles, InstructorNames, SesionTypes, CourseDescriptions }) => {
-    const router = useRouter()
-
-    const [isSelected, setIsSelected] = useState(false)
-
-
+    FormLabel,
+  } from '@mui/material';
+  import { color } from '@mui/system';
+  
+  import LibraryAddCheckIcon from '@mui/icons-material/LibraryAddCheck';
+  
+  import React, { useEffect, useState } from 'react';
+  import Router, { useRouter } from 'next/router';
+  import { collection, doc, updateDoc } from 'firebase/firestore';
+  import { db } from '../Firebase/Firebase';
+  
+  const UpdateForm = ({
+    ids,
+    titles,
+    InstructorNames,
+    SessionTypes,
+    CourseDescriptions,
+  }) => {
+    const router = useRouter();
+  
+    const [isSelected, setIsSelected] = useState(false);
+  
     const [formData, setFormData] = useState({
-        CourseTitle: titles,
-        InstructorName: InstructorNames,
-        CourseDescription: CourseDescriptions,
-        SesionType: SesionTypes,
-        img: ''
-
-
-    })
-
-
-
-    const handelChange = async (event) => {
-        setFormData(pre => {
-            return {
-                ...pre,
-                [event.target.name]: event.target.value
-            }
-
-        })
-    }
-
-    const handelSub = async (e) => {
-        e.preventDefault()
-
-        let collectedData = {
-            CourseDescription: formData.CourseDescription,
-            CourseTitle: formData.CourseTitle,
-            InstructorName: formData.InstructorName,
-            SesionType: formData.SesionType
-
-        }
-        try {
-            const res = await fetch(`http://localhost:1337/api/courses/${ids}`,
-                {
-                    headers: {
-                        'Content-Type': "application/json"
-                    },
-                    body: JSON.stringify({ data: collectedData }),
-                    method: "PUT",
-
-                }
-            )
-
-
-
-            router.reload()
-
-
-        } catch (err) {
-            console.log(err)
-        }
-
-
-    }
-
-    
-
-
-
+      CourseTitle: titles,
+      InstructorName: InstructorNames,
+      CourseDescription: CourseDescriptions,
+      SessionType: SessionTypes,
+      img: '',
+    });
+  
+    const handleChange = async (event) => {
+      setFormData((pre) => {
+        return {
+          ...pre,
+          [event.target.name]: event.target.value,
+        };
+      });
+    };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      let collectedData = {
+        CourseDescription: formData.CourseDescription,
+        CourseTitle: formData.CourseTitle,
+        InstructorName: formData.InstructorName,
+        SessionType: formData.SessionType,
+      };
+  
+      try {
+        const courseRef = doc(db, 'Course', ids);
+        await updateDoc(courseRef, collectedData);
+        router.reload();
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
     return (
-
-        <form onSubmit={handelSub} style={{
-            margin: "10px 10px 10px 10px"
-
-        }}>
-
-
-            <TextField
-
-                id="outlined-error-helper-text"
-                label="Course Title"
-                sx={{ margin: "10px 10px 10px 10px" }}
-                name="CourseTitle"
-                onChange={handelChange}
-                value={formData.CourseTitle}
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          margin: '10px 10px 10px 10px',
+        }}
+      >
+        <TextField
+          id="outlined-error-helper-text"
+          label="Course Title"
+          sx={{ margin: '10px 10px 10px 10px' }}
+          name="CourseTitle"
+          onChange={handleChange}
+          value={formData.CourseTitle}
+        />
+        <TextField
+          id="outlined-error-helper-text"
+          label="Instructor Name "
+          sx={{ margin: '10px 10px 10px 10px' }}
+          name="InstructorName"
+          onChange={handleChange}
+          value={formData.InstructorName}
+        />
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Course Description"
+          multiline
+          maxRows={4}
+          sx={{ margin: '10px 10px 10px 10px' }}
+          name="CourseDescription"
+          onChange={handleChange}
+          value={formData.CourseDescription}
+        />
+  
+        <FormControl sx={{ margin: '0 0 0 10px' }}>
+          <FormLabel
+            id="demo-row-radio-buttons-group-label"
+            sx={{ margin: '10px 10px 0px 20px' }}
+          >
+            Session Type
+          </FormLabel>
+  
+          <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label">
+            <FormControlLabel
+              value="Private"
+              name="SessionType"
+              control={<Radio />}
+              label="Private"
+              sx={{ margin: '0px 10px 0px 10px' }}
+              onChange={handleChange}
             />
-            <TextField
-                id="outlined-error-helper-text"
-                label="Instructor Name "
-                sx={{ margin: "10px 10px 10px 10px" }}
-                name="InstructorName"
-                onChange={handelChange}
-                value={formData.InstructorName}
+            <FormControlLabel
+              value="Public"
+              name="SessionType"
+              control={<Radio />}
+              label="Public"
+              sx={{ margin: '0px 10px 0px 10px' }}
+              onChange={handleChange}
+              />
+              </RadioGroup>
+              </FormControl>
+              <div></div>
 
+<div
+  style={{
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    margin: '10px 0 0 10px ',
+  }}
+>
+  {isSelected ? (
+    <LibraryAddCheckIcon sx={{ color: '#1ABC9C' }} />
+  ) : (
+    ''
+  )}
 
-            />
-            <TextField
-                id="outlined-multiline-flexible"
-                label="Course Description"
-                multiline
-                maxRows={4}
-                sx={{ margin: "10px 10px 10px 10px" }}
-                name="CourseDescription"
-                onChange={handelChange}
-                value={formData.CourseDescription}
+  {isSelected ? (
+    <span style={{ color: '#B2BABB', fontWeight: '500' }}>
+      File Selected
+    </span>
+  ) : (
+    ''
+  )}
+</div>
 
+<div style={{ display: 'flex' }}>
+  <Button
+    variant="outlined"
+    sx={{ margin: '10px 0 0 10px' }}
+    type="submit"
+  >
+    Update
+  </Button>
+</div>
+</form>
+);
+};
 
-            />
-
-            <FormControl sx={{ margin: "0 0 0 10px" }} >
-                <FormLabel id="demo-row-radio-buttons-group-label" sx={{ margin: "10px 10px 0px 20px" }}
-                >Sesion Type </FormLabel>
-
-                <RadioGroup
-                    row
-                    aria-labelledby="demo-row-radio-buttons-group-label"
-                    name="row-radio-buttons-group"
-
-                >
-
-                    <FormControlLabel value="Private" name="SesionType" control={<Radio />} label="Private" sx={{ margin: "0px 10px 0px 10px" }} onChange={handelChange} />
-                    <FormControlLabel value="Public" name="SesionType" control={<Radio />} label="Public" sx={{ margin: "0px 10px 0px 10px" }} onChange={handelChange} />
-
-                </RadioGroup>
-            </FormControl>
-            <div>
-
-
-            </div>
-
-            <div style={{ display: "flex", justifyContent: "flex-start", alignItems: "center", margin: "10px 0 0 10px " }}>
-                {isSelected ? <LibraryAddCheckIcon sx={{ color: "#1ABC9C" }} /> : ""}
-
-                {isSelected ? <span style={{ color: "#B2BABB", fontWeight: "500" }}>File Selected</span> : ""}
-
-            </div>
-
-            <div style={{ display: "flex" }}>
-                <Button variant="outlined" sx={{ margin: "10px 0 0 10px" }} type="submit">
-                    Update
-                </Button>
-            </div>
-
-
-
-        </form >
-
-
-
-
-
-
-    )
-}
-
-export default UpdateForm
+export default UpdateForm;
